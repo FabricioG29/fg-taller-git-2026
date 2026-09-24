41ff2d5755f399aa9987c871cf9ed10f62c4da4f
FabricioG29 <fabrigonza2003@gmail.com>
2026-09-24 21:11:45 +0000

    chore: Spring Boot API REST desde start.spring.io

---
+++ src/main/java/py/edu/uc/lp3/FgTallerGit2026Application.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgTallerGit2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgTallerGit2026Application.class, args);
+	}
+
+}
